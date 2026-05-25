b2da6b3c806bf2ddfa8e967ecd8796b7ef3f1028
vashtera <raul.aytbaev@gmail.com>
2026-05-25 21:05:52 +0500

    added states

--- handlers/registration.py
+++ handlers/registration.py
@@ -1,13 +1,19 @@
 from aiogram import F, Router
-from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import Message, CallbackQuery
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import StatesGroup, State
 
 import App.keyboards as kb
 from App.database.requests import get_user_by_tg_id
 
 router = Router()
 
+class Register(StatesGroup):
+    price = State()
+    start_date = State()
+
 @router.callback_query(F.data == "registration")
-async def add_new_user():
+async def add_new_user(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
 
 
