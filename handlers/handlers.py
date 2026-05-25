8cd722e8365a1ac01336b6db586c49a9d43d0f20
vashtera <raul.aytbaev@gmail.com>
2026-05-25 14:36:43 +0500

    test func

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,6 +1,6 @@
 from aiogram import F, Router
 from aiogram.filters import Command
-from aiogram.types import Message
+from aiogram.types import CallbackQuery, Message
 
 router = Router()
 
@@ -8,5 +8,10 @@ router = Router()
 @router.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
     await message.answer(f"Привет {message.from_user.full_name}\n я трекер твоего отказа от курения")
+
+@router.callback_query(F.data == "profile")
+async def show_profile(callback: CallbackQuery):
+    await callback.answer("Вы выбрали профиль")
+    await callback.message.answer(f"Hi {callback.from_user.full_name}")
 
 
