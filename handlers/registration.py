131a57bf6b3f7fe47669acf5fa93b536f2498cf9
vashtera <raul.aytbaev@gmail.com>
2026-05-25 21:16:43 +0500

    added FSM

--- handlers/registration.py
+++ handlers/registration.py
@@ -15,5 +15,23 @@ class Register(StatesGroup):
 @router.callback_query(F.data == "registration")
 async def add_new_user(callback: CallbackQuery, state: FSMContext):
     await callback.answer()
+    await state.set_state(Register.price)
+    await callback.message.answer("Введите стоимость одной пачки сигарет")
+
+@router.message(Register.price)
+async def register_price(message: Message, state: FSMContext):
+    await state.update_data(price=message.text)
+    await state.set_state(Register.start_date)
+    await message.answer("Введите дату, когда ты бросил курить в формате ДД.ММ.ГГГГ")
+
+@router.message(Register.start_date)
+async def register_date(message: Message, state: FSMContext):
+    data = await state.get_data()
+    user_price = data.get("price")
+    user_date = message.text
+    user_id = message.from_user.id
+    user_name = message.from_user.first_name
+    await add_new_user
+                                  
 
 
