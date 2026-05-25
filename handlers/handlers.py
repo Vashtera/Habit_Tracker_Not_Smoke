32688db0e018fabe0c0cd95ea3ef1f4bbf79ce0a
vashtera <raul.aytbaev@gmail.com>
2026-05-25 18:27:04 +0500

    Added show profile func for test

--- handlers/handlers.py
+++ handlers/handlers.py
@@ -1,17 +1,24 @@
 from aiogram import F, Router
 from aiogram.filters import Command
 from aiogram.types import CallbackQuery, Message
+import App.keyboards as kb
 
 router = Router()
 
-# Command handler
+# This command shows the keyboard only for existing users(NEED TO CHANGE)
 @router.message(Command("start"))
 async def command_start_handler(message: Message) -> None:
-    await message.answer(f"Привет {message.from_user.full_name}\n я трекер твоего отказа от курения")
+    await message.answer(f"Привет {message.from_user.full_name}\n я трекер твоего отказа от курения",
+                         reply_markup=kb.keyboard_for_excisting_user)
 
+# This async func shows a profile of user
 @router.callback_query(F.data == "profile")
 async def show_profile(callback: CallbackQuery):
     await callback.answer("Вы выбрали профиль")
-    await callback.message.answer(f"Hi {callback.from_user.full_name}")
+    await callback.message.answer(f"Привет {callback.from_user.full_name}!")
+    
+
+
+
 
 
