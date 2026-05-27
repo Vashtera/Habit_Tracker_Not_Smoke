fbf83f1515db70e8f22986371189d1149c17ced7
vashtera <raul.aytbaev@gmail.com>
2026-05-27 15:30:17 +0500

    started func with new price func

--- handlers/changes.py
+++ handlers/changes.py
@@ -7,5 +7,21 @@ from App.database.requests import get_user_by_tg_id
 
 router = Router()
 
-@router.callback_query(F.data == 'change'):
-async def 
+class Change(StatesGroup):
+    new_price = State()
+
+@router.callback_query(F.data == 'change')
+async def add_new_price(callback: CallbackQuery, state: FSMContext):
+    await callback.answer()
+    await state.set_state(Change.new_price)
+    await callback.message.answer("Введите новую стоимость пачки сигарет")
+
+@router.message(Change.new_price)
+async def change_with_new_price(message: Message, state: FSMContext):
+    new_price = float(message.text)
+    user = await get_user_by_tg_id(message.from_user.id)
+    user_old_date = user[4]
+    user_old_price = user[3]
+    user_cig_in_pack = user[6]
+    user_cig_per_day = user[7]
+    user_saved_money = user[5]
