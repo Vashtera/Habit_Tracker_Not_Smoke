a4b6f0acc4437b4e3d51836dbf75cfabd8184df9
vashtera <raul.aytbaev@gmail.com>
2026-05-27 16:38:37 +0500

    connected router

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -3,9 +3,11 @@ from aiogram import Dispatcher
 from .start import router as start_router
 from .registration import router as reg_router
 from .profile import router as profile_router
+from .changes import router as change_router
 
 
 def register_routers(dp: Dispatcher):
     dp.include_router(start_router)
     dp.include_router(reg_router)
     dp.include_router(profile_router)
+    dp.include_router(change_router)
